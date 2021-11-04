2a5154ab6d66ceae9a68c0d3766975052ea72d56
Kanad <kanadsante@gmail.com>
2021-11-04 12:50:19 +0530

    update comment on application class

--- src/main/java/com/simpledev/questionservice/QuestionServiceApplication.java
+++ src/main/java/com/simpledev/questionservice/QuestionServiceApplication.java
@@ -3,9 +3,16 @@ package com.simpledev.questionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 1st way to start this application
 @SpringBootApplication
-// No need of @EnableDiscoveryClient annotation anymore for service to register with consul agent.
-// Just include dependency.
+
+//2nd way to start this application
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+
+
+// No need of @EnableDiscoveryClient annotation anymore for service to register with consul agent. Just include "spring-cloud-starter-consul-discovery" dependency. 
 // Now Consul's discovery client implementation is included under @EnableAutoConfiguration (which is under @SpringBootApplication).
 public class QuestionServiceApplication {
 
